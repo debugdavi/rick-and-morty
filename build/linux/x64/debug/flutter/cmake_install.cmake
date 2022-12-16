1aa5734b409107c8b4c77f850834b12f28d41605
dathhll <93881880+dathhll@users.noreply.github.com>
2022-12-16 14:05:58 -0300

    Final

--- build/linux/x64/debug/flutter/cmake_install.cmake
+++ build/linux/x64/debug/flutter/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: /home/luvas/Área de trabalho/PDM/apps/1/rick_and_morty/linux/flutter
+# Install script for directory: /home/luvas/Área de trabalho/PDM/apps/rick-and-morty/linux/flutter
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "/home/luvas/Área de trabalho/PDM/apps/1/rick_and_morty/build/linux/x64/debug/bundle")
+  set(CMAKE_INSTALL_PREFIX "/home/luvas/Área de trabalho/PDM/apps/rick-and-morty/build/linux/x64/debug/bundle")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
